03066fb10272961b0c668d556535ca77f1aa6545
sikB <sikhovski@icloud.com>
2016-04-27 13:35:42 -0400

    update search with array of names

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -2,8 +2,33 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+router.post('/search', function(req, res, next) {
+	var name = req.body.name;
+
+	var dcClass = [
+		'Tristan',
+		'Josh',
+		"Bogdan",
+		'Keith',
+		'Will',
+		'Curtis',
+		'Joe',
+		'Kochan',
+		'Patric',
+		'Jonathan',
+		'Jeremy'
+	];
+
+	if(dcClass.indexOf(name) > -1){
+		res.json({message:'hello ' + name + ', you are a student in this class'})
+	}else{
+		res.json({message: 'you are not in the clas'})
+	}
+  // res.json({message: req.body.name});
 });
+
 
 module.exports = router;
--- frontEnd/controller.js
+++ frontEnd/controller.js
@@ -3,5 +3,14 @@ var searchApp = angular.module('searchApp', []);
 searchApp.controller('searchController', function($scope, $http){
 	$scope.search = function(){
 		$scope.message = 'wasup';
-	};
-});
+
+		var apiUrl = 'http://localhost:3000/search';
+		$http.post(apiUrl, {name : $scope.who}).then(function successCallback(response){
+			$scope.result = response.data.message;
+			$scope.status = response.status;
+			},
+			function errorCallback(response){
+				$scope.result = 'ERROR';
+			})
+		}
+	});
