bd2db678f7ea7d3592571f8780179c0e733daf3d
sikB <sikhovski@icloud.com>
2016-04-27 14:29:04 -0400

    made new route for /search
    /search for local host and for the html file.
    local host is the son array
    the html is the list of items that were added

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,32 +1,52 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+var mongoUrl = 'mongodb://localhost:27017/btb';
+
+var db;
+MongoClient.connect(mongoUrl, function(eror, database){
+	db = database;
+})
 
 /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
+router.get('/search', function(req, res, next) {
+	db.collection('students').find().toArray(function(error, studentResult){
+		res.json(studentResult);
+	});
+  
+});
 router.post('/search', function(req, res, next) {
-	var name = req.body.name;
+	var individualStudent = req.body.name;
+	db.collection('students').find({name: individualStudent}).toArray(function(error, studentResult){
+		if(studentResult.length == 0){
+			db.collection('students').insertOne({
+				name: individualStudent
+			})
+			res.json('Sorry, not found. Added ' + individualStudent + ' to the database')
+		}else{
+			res.json('Student: ' + studentResult[0].name)
+		}
+	});
 
-	var dcClass = [
-		'Tristan',
-		'Josh',
-		"Bogdan",
-		'Keith',
-		'Will',
-		'Curtis',
-		'Joe',
-		'Kochan',
-		'Patric',
-		'Jonathan',
-		'Jeremy'
-	];
+	// var dcClass = [
+	// 	'Tristan',
+	// 	'Josh',
+	// 	"Bogdan",
+	// 	'Keith',
+	// 	'Will',
+	// 	'Curtis',
+	// 	'Joe',
+	// 	'Kochan',
+	// 	'Patric',
+	// 	'Jonathan',
+	// 	'Jeremy'
+	// ];
 
-	if(dcClass.indexOf(name) > -1){
-		res.json({message:'hello ' + name + ', you are a student in this class'})
-	}else{
-		res.json({message: 'you are not in the clas'})
-	}
+	// if(dcClass.indexOf(name) > -1){
+	// 	res.json({message:'hello ' + name + ', you are a student in this class'})
+	// }else{
+	// 	res.json({message: 'you are not in the clas'})
+	// }
   // res.json({message: req.body.name});
 });
 
--- frontEnd/controller.js
+++ frontEnd/controller.js
@@ -1,12 +1,23 @@
 var searchApp = angular.module('searchApp', []);
 
 searchApp.controller('searchController', function($scope, $http){
+
+	var apiUrl = 'http://localhost:3000/search';
+		$http.get(apiUrl, {name : 'ALL'}).then(function successCallback(response){
+			$scope.studentsOnLoad = response.data;
+			$scope.status = response.status;
+			},
+			function errorCallback(response){
+				$scope.result = 'ERROR';
+			})
+
+
 	$scope.search = function(){
 		$scope.message = 'wasup';
 
 		var apiUrl = 'http://localhost:3000/search';
 		$http.post(apiUrl, {name : $scope.who}).then(function successCallback(response){
-			$scope.result = response.data.message;
+			$scope.result = response.data;
 			$scope.status = response.status;
 			},
 			function errorCallback(response){
